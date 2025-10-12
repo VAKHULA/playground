4be03506369079134c04b841f31e6279f3ae5220
VAKHULA <andrii.vakhula@gmail.com>
2025-10-12 15:17:50 +0200

    upd page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+import { Link } from '@/components/link';
+import { Button } from '@/components/button';
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <p>
+        <Link href="#">link</Link>
+      </p>
+      <p>
+        <Button onClick={() => {}}>Button</Button>
+        <br />
+        <Button className="secondary" onClick={() => {}}>
+          Button
+        </Button>
+        <br />
+        <Button className="contrast" onClick={() => {}}>
+          Button
+        </Button>
+        <br />
+      </p>
+    </div>
+  );
 }
