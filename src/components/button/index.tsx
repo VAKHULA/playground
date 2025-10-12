733eb4769c5b00beb12d6a35501cfb1d5e7d652e
VAKHULA <andrii.vakhula@gmail.com>
2025-10-12 15:17:26 +0200

    add button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,8 +1,10 @@
+import './style.css';
 type ButtonProps = {
   children: React.ReactNode;
-  onClick?: () => void;
+  onClick: () => void;
+  className?: 'secondary' | 'contrast';
 };
 
-export const Button: React.FC<ButtonProps> = ({ children }) => {
-  return <button>{children}</button>;
+export const Button: React.FC<ButtonProps> = ({ children, className = '' }) => {
+  return <button className={className}>{children}</button>;
 };
