913cf1bd4f32358b9008e7cc23ce3d9b327013c1
VAKHULA <andrii.vakhula@gmail.com>
2025-10-12 12:40:12 +0200

    Updates

---
+++ assets/Button.stories-C5OD_Y9G.js
@@ -0,0 +1,6 @@
+import{j as e}from"./jsx-runtime-CcwSNGrQ.js";import"./iframe-YaFJfY7Q.js";import"./preload-helper-PPVm8Dsz.js";const t=({children:o})=>e.jsx("button",{children:o});t.__docgenInfo={description:"",methods:[],displayName:"Button"};const m={component:t},r={args:{primary:!0,label:"Button"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...r.parameters?.docs?.source}}};const p=["Primary"];export{r as Primary,p as __namedExportsOrder,m as default};
---
+++ assets/jsx-runtime-CcwSNGrQ.js
@@ -0,0 +1,1 @@
+import{r}from"./iframe-YaFJfY7Q.js";var s=r();export{s as j};
